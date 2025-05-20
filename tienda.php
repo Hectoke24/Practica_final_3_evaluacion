f91256ffd8811f85e2cb6fcbd1559856b6d05bb5
hector galan <hectorgaalan2006@gmail.com>
2025-05-20 17:28:44 +0200

    Modificacion de codigos de tienda y creacion menu.html

--- tienda.php
+++ tienda.php
@@ -12,7 +12,7 @@ require_once("conexion.php");
 
 <head>
     <meta charset="UTF-8">
-    <title>Tienda de Zapatillas</title>
+    <title>H-KICKS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .imagen-producto {
@@ -25,34 +25,7 @@ require_once("conexion.php");
 
 <body class="bg-light">
 
-    <!-- CABECERA CON LOGO Y MENÚ -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="Imagenes/logo.jpg" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
-                Mi Tienda Zapatillas
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="tienda.php">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Productos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contacto</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-danger" href="logout.php">Cerrar sesión</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    
 
     <div class="container mt-4">
         <h2 class="mb-4">Bienvenido, <?php echo $_SESSION['usuario']; ?> 👟</h2>
